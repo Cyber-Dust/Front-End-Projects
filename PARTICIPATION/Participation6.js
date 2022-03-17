e4d551a88c77921cffbca80b8110a805648029bc
Dasean Volk <dvolkx3@gmail.com>
2022-03-16 23:46:55 -0700

    added p response

--- PARTICIPATION/Participation6.js
+++ PARTICIPATION/Participation6.js
@@ -1,8 +1,4 @@
-// Week 6 - February 15th
-// karljian@usc.edu
-// Karl Jiang
-// dvolk@usc.edu
-// Dasean Volk
+// 6
 
 if (/{6,12}/.test(password) && /[a-zA-Z0-9_]/.test(password) && /\d{2}/.test(password)) {
     console.log('Secure password');
